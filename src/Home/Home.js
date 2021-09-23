9167a4148249cb5ed64c5a23199140d2e9186f0f
ishubham21 <sg2199203@gmail.com>
2021-09-23 18:07:55 +0530

    adding styles

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,9 +6,7 @@ const Home = () => {
     return ( 
         <div className="home-container">
             <div className="home-wrapper">
-                
                 <ImgContainer />
-
                 <DescContainer />
             </div>
         </div>
