94d8c5dbc4a6082b1b8bd5ad819ab5d0b4a12c20
ishubham21 <sg2199203@gmail.com>
2021-09-26 11:16:24 +0530

    homepage desktop view p-1

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -2,25 +2,27 @@ import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 const Navbar = () => {
-    return(
+    return (
         <nav className="navbar">
-            <div className="nav-title-container">
-                <div className="nav-blk"></div>
-                <p className="nav-name">
-                    Shubham Gautam  
-                </p>
-                <p className="slash">
-                    /
-                </p>
-                <p className="nav-designation">
-                    Software Developer
-                </p>
-            </div>
-            <div className="nav-link-container">
-                <Link to='/about' className="nav-links">About Me</Link>
-                <Link to='/resume' className="nav-links">Resume</Link>
-                <Link to='/projects' className="nav-links">Projects</Link>
-                <Link to='/contact' className="nav-links">Contact</Link>
+            <div className="nav-sub-container">
+                <div className="nav-title-container">
+                    <div className="nav-blk"></div>
+                    <h2 className="nav-name">
+                        Shubham Gautam
+                    </h2>
+                    <p className="slash">
+                        /
+                    </p>
+                    <p className="nav-designation">
+                        Software Developer
+                    </p>
+                </div>
+                <div className="nav-link-container">
+                    <Link to='/about' className="nav-links">About Me</Link>
+                    <Link to='/resume' className="nav-links">Resume</Link>
+                    <Link to='/projects' className="nav-links">Projects</Link>
+                    <Link to='/contact' className="nav-links">Contact</Link>
+                </div>
             </div>
         </nav>
     )
