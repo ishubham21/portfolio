c05e6e60f5f266bb5c97631f2661de7d28831ac5
ishubham21 <sg2199203@gmail.com>
2021-10-02 13:10:36 +0530

    setting up SMTP server

--- Server/index.js
+++ Server/index.js
@@ -4,56 +4,29 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 4000;
 const bodyParser = require('body-parser')
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-//nodemailer setup
-const nodemailer = require('nodemailer')
-const dotenv = require('dotenv')
-dotenv.config()
+//importing the mail_config to send the emails
+const sendMail = require('./mail_config/mail_config')
 
+//making use of the express router
+const routes = require('./routes')
+app.use('/', routes);
 
 //asking the express server to read text/plain also since it only process application/json by default
 app.use(express.json({
     type: ['application/json', 'text/plain']
 }))
 
-//using body parser to fetch the body of the requests 
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-const sendEmail = (userDetails) => {
-    //configuring the gmail SMTP server using createTransport
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.USER_EMAIL,
-            pass: process.env.USER_PASS
-        }
-    })
-
-    let mailOptions = {
-        from: 'ishubham2101@gmail.com',
-        bcc: `${userDetails.userEmail}, sg2199203@gmail.com`,
-        subject: 'Bonjour!',
-        html: `Hey, ${userDetails.userName}! Thank you for contacting me. Your email has been recieved, and I will soon reply back. <br> Meanwhile, just to confirm, here are your details.  <br> <strong> Your Name </strong> - ${userDetails.userName} <br> <strong> Your Email </strong> - ${userDetails.userEmail} <br> <strong> Your Message </strong> - ${userDetails.userMsg}.`
-    }
-
-    transporter.sendMail(mailOptions)
-        .then((res) => {
-            console.log('Email is sent');
-        })
-        .catch((err) => {
-            console.log('Error - ' + err);
-        })
-}
-
 //post request to send an email
 app.post('/sendMail', urlencodedParser, (req, res) => {
-    sendEmail(req.body)
-    res.send('Received the data')
+    
+    //sending the details recieved from the POST req as params 
+    sendMail(req.body.userName, req.body.userEmail, req.body.userMsg)
+    
+    //sending a positive response upon completion
+    res.status(200).send('Email has been sent')
 })
-
-//making use of the express router
-const routes = require('./routes')
-app.use('/', routes);
 
 app.listen(port, function () {
     console.log("Listening on port " + port);
