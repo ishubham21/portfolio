9ee9b7778802c8ec0768c2113b9a7b759025b7f5
Shubham Gautam <34435822+ishubham21@users.noreply.github.com>
2022-07-05 12:13:19 +0530

    Update DescContainer.js

--- src/components/Home/Desc-container/DescContainer.js
+++ src/components/Home/Desc-container/DescContainer.js
@@ -18,10 +18,10 @@ const DescContainer = () => {
                 </Link>
             </div>
             <div className="bio-paragraph">
-                I'm an engineering undergrad and a programmer. I work as a Web Developer with experience in handling JavaScript, Node, React and Angular coding, designs and extensions.
+                I'm an engineering undergrad and a programmer. I work as a Full-Stack Web Developer with experience in handling JavaScript, Node, React and Angular coding, designs and extensions.
             </div>
             <div className="bio-paragraph">
-                I've been an MLH Fellow (Summer' 21) and I'm also a content writer with a versatile style of writing, working as a Technical Content Writer at InterviewBit.
+                I've been an MLH Fellow (Summer' 21) and I love developing software solutions and tackling challenges using new technologies. I'm currently an SDE Intern at Warmspace. Oh also, I love commuities! I Have been a Lead at Google DSC, Postman Student Community, and an MLSA :)
             </div>
         </div>
     )
